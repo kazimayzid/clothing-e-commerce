1bb31d103cb417bff064ead80c9c044588929093
kazimayzid <kazimayzid208@gmail.com>
2026-08-19 23:33:00 +0600

    more functionality added to product api

--- app/api/v1/products/route.ts
+++ app/api/v1/products/route.ts
@@ -2,13 +2,27 @@ import { mockProducts20 } from "@/Data/mockProducts";
 import type { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const queryGender = searchParams.get("gender");
+  const querySearch = searchParams.get("search");
 
-    const searchParams = request.nextUrl.searchParams
-    const query = searchParams.get('category')
-    
-    const queryData = query ? mockProducts20.filter((product) => product.gender.includes(query)) : mockProducts20
-    console.log(queryData, 'hekko');
-    
-    
-    return Response.json(queryData)
+  let products = mockProducts20;
+
+  if (queryGender) {
+    products = products.filter((product) =>
+      product.gender.includes(queryGender),
+    );
+  }
+
+  if (querySearch) {
+    products = products.filter(
+      (product) =>
+        product.title.toLowerCase().includes(querySearch.toLowerCase()) ||
+        product.tags?.some((tag) =>
+          tag.toLowerCase().includes(querySearch.toLowerCase()),
+        ),
+    );
+  }
+
+  return Response.json(products);
 }
